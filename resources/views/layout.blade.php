1f6860613f7a073f4e419be97ed3fadd4db7f542
Brad Traversy <traversymedia@gmail.com>
2024-09-10 12:30:06 -0400

    Tailwind setup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 
