1ac998afca2156b4e064e8a7bc8f139abebe07d9
Brad Traversy <traversymedia@gmail.com>
2024-09-12 17:29:18 -0400

    Homepage jobs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
-  <h1>Welcome To Workopia</h1>
+  <h2 class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Welcome To Workopia</h2>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+    @forelse($jobs as $job)
+    <x-job-card :job="$job" />
+    @empty
+    <p>No jobs available</p>
+    @endforelse
+  </div>
+  <a href="{{route('jobs.index')}}" class="block text-xl text-center">
+    <i class="fa fa-arrow-alt-circle-right"></i> Show All Jobs
+  </a>
 
   <x-bottom-banner />
 </x-layout>
